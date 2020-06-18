257d791437e3952e97835a2500e9966380828c8f
kongyi <jerrykongyi1997@gmail.com>
2020-06-18 23:39:30 +0800

    div画圈

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let div = document.getElementById("canvas");
-
+let painting = false;
 // 鼠标按下去
 div.onmousedown = (a) => {
+  painting = true;
   let x = a.clientX;
   let y = a.clientY;
   let divA = document.createElement("div");
@@ -17,6 +18,25 @@ div.onmousedown = (a) => {
   div.appendChild(divA);
 };
 // 鼠标移动
-div.onmousemove = () => {};
+div.onmousemove = (a) => {
+  if (painting) {
+    let x = a.clientX;
+    let y = a.clientY;
+    let divA = document.createElement("div");
+    divA.style =
+      "width: 6px; height: 6px; background: red;" +
+      "border-radius: 3px; position: absolute;" +
+      "left:" +
+      (x - 3) +
+      "px;" +
+      "top:" +
+      (y - 3) +
+      "px;";
+    div.appendChild(divA);
+  } else {
+  }
+};
 // 鼠标松开
-div.onmouseup = () => {};
+div.onmouseup = () => {
+  painting = false;
+};
