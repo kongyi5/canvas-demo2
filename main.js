901ae1f6b440c43d7c574925506a9e2f84bc0ce5
kongyi <jerrykongyi1997@gmail.com>
2020-06-19 09:19:29 +0800

    canvas画线、全屏canvas

--- main.js
+++ main.js
@@ -1,42 +1,65 @@
-let div = document.getElementById("canvas");
+let yyy = document.getElementById("xxx");
+let ctx = yyy.getContext("2d");
+
+xxx();
+
+window.onresize = () => {
+  xxx();
+};
+
 let painting = false;
-// 鼠标按下去
-div.onmousedown = (a) => {
+let lastPoint = {
+  x: undefined,
+  y: undefined,
+};
+
+yyy.onmousedown = (a) => {
   painting = true;
   let x = a.clientX;
   let y = a.clientY;
-  let divA = document.createElement("div");
-  divA.style =
-    "width: 6px; height: 6px; background: red;" +
-    "border-radius: 3px; position: absolute;" +
-    "left:" +
-    (x - 3) +
-    "px;" +
-    "top:" +
-    (y - 3) +
-    "px;";
-  div.appendChild(divA);
+  // 相对于视口的位置
+  lastPoint = {
+    x: x,
+    y: y,
+  };
 };
-// 鼠标移动
-div.onmousemove = (a) => {
+yyy.onmousemove = (a) => {
   if (painting) {
     let x = a.clientX;
     let y = a.clientY;
-    let divA = document.createElement("div");
-    divA.style =
-      "width: 6px; height: 6px; background: red;" +
-      "border-radius: 3px; position: absolute;" +
-      "left:" +
-      (x - 3) +
-      "px;" +
-      "top:" +
-      (y - 3) +
-      "px;";
-    div.appendChild(divA);
+    // 相对于视口的位置
+    let newPoint = {
+      x: x,
+      y: y,
+    };
+    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+    lastPoint = newPoint;
   } else {
   }
 };
-// 鼠标松开
-div.onmouseup = () => {
+yyy.onmouseup = (a) => {
   painting = false;
 };
+
+function drawCircle(x, y, radius) {
+  ctx.beginPath();
+  ctx.strokeStyle = "black";
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 5;
+  ctx.moveTo(x1, y1); // 起点
+  ctx.lineTo(x2, y2); // 终点
+  ctx.stroke();
+  ctx.closePath();
+}
+function xxx() {
+  let pageWidth = document.documentElement.clientWidth;
+  let pageHeight = document.documentElement.clientHeight;
+  yyy.width = pageWidth;
+  yyy.height = pageHeight;
+}
